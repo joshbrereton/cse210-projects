8c0d1c54635ab7281fa74aa79dd9c18009221905
josh brereton <joshbrereton@hotmail.com>
2022-06-11 23:50:24 -0600

    setup complete

--- cse210-projects/tic-tac-toe.py
+++ cse210-projects/tic-tac-toe.py
@@ -1,3 +1,4 @@
+print()
 print(f' 1 | 2 | 3')
 print()
 print(f'--++--++--++')
@@ -8,3 +9,10 @@ print(f'--++--++--++')
 print()
 print(f' 7 | 8 | 9')
 print()
+first_x = input(f"x's turn to choose a square (1-9) ")
+first_0 = input(f"o's turn to choose a square (1-9) ")
+
+
+
+
+print('Good game. Thanks for playing!')
